c38a2316c7391e68704f20ba72666d12e0fd8a71
조경진 <kj_cho@idealidea.co.kr>
2020-07-18 12:07:17 +0900

    Glide로 웹상의 이미지를 바로 띄워주기

--- app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/library_20200718/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.library_20200718
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -26,6 +27,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        val imageUrl = "https://i.pinimg.com/originals/c6/58/b9/c658b98157dec19603499b76d519e104.jpg"
+
+        Glide.with(mContext).load(imageUrl).into(profileImg)
 
     }
 
